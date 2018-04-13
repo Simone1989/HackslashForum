48cba6b6a0164e91b6145eb124001cf07a9c6d2c
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2018-04-13 12:20:25 +0200

    Comment model complete

--- HackslashForum/HackslashForum/Models/Model Classes/Comment.cs
+++ HackslashForum/HackslashForum/Models/Model Classes/Comment.cs
@@ -1,6 +1,18 @@
-﻿namespace HackslashForum
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackslashForum
 {
     public class Comment
     {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public DateTime DateTimeCommentMade { get; set; }
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
+        public User User { get; set; }
+        public Post Post { get; set; }
     }
 }
