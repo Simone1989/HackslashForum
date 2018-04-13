f4062dec51eeaa2d0a0a4b0bb83d501761601ade
Simone Björklund <simone_1000@hotmail.com>
2018-04-13 11:27:47 +0200

    Added properties in ApplicationUser model

--- HackslashForum/HackslashForum/Data/Migrations/20180413092203_Added properties in ApplicationUser model.Designer.cs
+++ HackslashForum/HackslashForum/Data/Migrations/20180413092203_Added properties in ApplicationUser model.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace HackslashForum.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180413092203_Added properties in ApplicationUser model")]
+    partial class AddedpropertiesinApplicationUsermodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
