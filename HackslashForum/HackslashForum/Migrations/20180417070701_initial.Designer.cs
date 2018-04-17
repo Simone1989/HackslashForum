15c8ceaeef7cc8ddb0efa69d7c964310c1dbb0ad
Simone1989 <31948425+Simone1989@users.noreply.github.com>
2018-04-17 09:18:47 +0200

    Database up and running

--- HackslashForum/HackslashForum/Migrations/20180417070701_initial.Designer.cs
+++ HackslashForum/HackslashForum/Migrations/20180417070701_initial.Designer.cs
@@ -8,12 +8,13 @@ using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 
-namespace HackslashForum.Data.Migrations
+namespace HackslashForum.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180417070701_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
