34e0c9a8b2ca361af9326be66bdea74e5b7b200f
Johan Svensson <johan.svensson93@outlook.com>
2018-04-18 14:21:10 +0200

    Fixed Login issue and lastlogin variable
    Lastlogin variable will now be set to current time. Now I need to fix where it updates.

--- HackslashForum/HackslashForum/Models/Model Classes/ApplicationUser.cs
+++ HackslashForum/HackslashForum/Models/Model Classes/ApplicationUser.cs
@@ -9,7 +9,7 @@ namespace HackslashForum.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public DateTime? LastLogin { get; set; }
+        public DateTime? LastLogin { get; set; } = DateTime.Now;
         public DateTime AccountCreationDate { get; set; }
         public string Location { get; set; }
         public string ProfilePicture { get; set; }
