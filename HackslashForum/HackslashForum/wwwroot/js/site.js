2fd611d7fac600fd59357daf52ebae941983d079
Simone Björklund <simone_1000@hotmail.com>
2018-05-07 09:53:01 +0200

    Found error in displaying images via api

--- HackslashForum/HackslashForum/wwwroot/js/site.js
+++ HackslashForum/HackslashForum/wwwroot/js/site.js
@@ -1,35 +1,27 @@
-﻿// Write your JavaScript code.
-
+﻿
 //events: https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=11.974560&page=20&text=tech&radius=50&lat=57.708870
 //grupper: https://api.meetup.com/find/groups?&sign=true&photo-host=public&country=se&upcoming_events=true&location=göteborg&text=tech&radius=50&page=10
 
 window.addEventListener('load', function (event) {
 
-	document.getElementById('getGroups').addEventListener('click', getGroups);
+	document.getElementById('getGroups');
 	document.getElementById('getEvents').addEventListener('click', getEvents);
+
+	getGroups();
 
 	// Fixa CORS
 	function getGroups() {
 		fetch('https://api.meetup.com/find/groups?&sign=true&photo-host=public&country=se&upcoming_events=true&location=göteborg&text=tech&radius=50&page=10&key=785d217213c1c192a7e71c4df2734')
 			.then(function (response) { return response.json(); })
 			.then(data => {
-				let outputGroups = '<h2>Grupper</h2>';
+				let outputGroups = '';
 				data.forEach(function (group) {
-					outputGroups +=
-						//<div>
-						//	<h4>${group.name}</h4>
-						//	${group.photo_link}
-						//	<p>${group.next_event.name}</p>
-						//	<p>${group.link}</p>
-						//	<p>${group.city}</p>
-						//</div>
-						`
-						<div class="card" style="width:400px">
-						  <img class="card-img-top" src=${group.photo_link} alt="Card image" style="width:100px">
-						  <div class="card-body">
+					outputGroups += `
+						<div>
 							<br>
-							<a href="${group.link}" class="btn btn-primary">${group.name}</a>
-						  </div>
+							<p>Namn: ${group.name}</p>
+							<p>Kommande meetup: ${group.next_event.name}</p>
+							<a href="${group.link}" class="btn btn-primary">Länk</a>
 							<br>
 						</div>
 					`;
@@ -37,10 +29,10 @@ window.addEventListener('load', function (event) {
 				});
 				document.getElementById('outputGroups').innerHTML = outputGroups;
 			})
-			.catch(function (error) {
-				console.log(JSON.stringify(error));
-				document.getElementById('output').innerHTML = JSON.stringify(error);
-			});
+		.catch(function (error) {
+			console.log(JSON.stringify(error));
+			document.getElementById('outputGroups').innerHTML = JSON.stringify(error);
+		});
 	}
 
 	function getEvents() {
