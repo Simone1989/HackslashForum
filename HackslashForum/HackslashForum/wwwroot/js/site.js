211bfc90a2c3703980e0354425dd59d3c82bd9ac
Simone Björklund <simone_1000@hotmail.com>
2018-05-07 15:18:02 +0200

    Trying cors

--- HackslashForum/HackslashForum/wwwroot/js/site.js
+++ HackslashForum/HackslashForum/wwwroot/js/site.js
@@ -1,13 +1,15 @@
-﻿
-//events: https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=11.974560&page=20&text=tech&radius=50&lat=57.708870
-//grupper: https://api.meetup.com/find/groups?&sign=true&photo-host=public&country=se&upcoming_events=true&location=göteborg&text=tech&radius=50&page=10
+﻿window.addEventListener('load', function (event) {
 
-window.addEventListener('load', function (event) {
+
+	//events: https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=11.974560&page=20&text=tech&radius=50&lat=57.708870
+	//grupper: https://api.meetup.com/find/groups?&sign=true&photo-host=public&country=se&upcoming_events=true&location=göteborg&text=tech&radius=50&page=10
+
 
 	document.getElementById('getGroups');
-	document.getElementById('getEvents').addEventListener('click', getEvents);
+	document.getElementById('getEvents');
 
 	getGroups();
+	getEvents();
 
 	// Fixa CORS
 	function getGroups() {
@@ -19,7 +21,7 @@ window.addEventListener('load', function (event) {
 					outputGroups += `
 						<div>
 							<br>
-							<p>Namn: ${group.name}</p>
+							<p span style="font-weight:bold">${group.name}</p>
 							<p>Kommande meetup: ${group.next_event.name}</p>
 							<a href="${group.link}" class="btn btn-primary">Länk</a>
 							<br>
@@ -29,20 +31,19 @@ window.addEventListener('load', function (event) {
 				});
 				document.getElementById('outputGroups').innerHTML = outputGroups;
 			})
-		.catch(function (error) {
-			console.log(JSON.stringify(error));
-			document.getElementById('outputGroups').innerHTML = JSON.stringify(error);
-		});
+			.catch(function (error) {
+				console.log(JSON.stringify(error));
+				document.getElementById('outputGroups').innerHTML = JSON.stringify(error);
+			});
 	}
 
 	function getEvents() {
-		fetch('https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=11.974560&page=20&text=tech&radius=50&lat=57.708870&key=785d217213c1c192a7e71c4df2734')
+		fetch('https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=11.974560&topic_category=tech&page=10&radius=50&lat=57.708870&key=785d217213c1c192a7e71c4df2734')
 			.then(function (response) { return response.json(); })
 			.then(data => {
 				let outputEvents = '<h2>Meetups</h2>';
 				data.forEach(function (event) {
-					outputEvents +=
-						`
+					outputEvents += `
 						<div>
 							<h4>${event.name}</h4>
 							<p>När: ${event.local_date}, kl. ${event.local_time}
@@ -53,10 +54,10 @@ window.addEventListener('load', function (event) {
 				});
 				document.getElementById('outputEvents').innerHTML = outputEvents;
 			})
-			//.catch(function (error) {
-			//	console.log(JSON.stringify(error));
-			//	document.getElementById('output').innerHTML = JSON.stringify(error);
-			//});
+			.catch(function (error) {
+				console.log(JSON.stringify(error));
+				document.getElementById('output').innerHTML = JSON.stringify(error);
+			});
 	}
 
 
