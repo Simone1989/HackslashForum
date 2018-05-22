d4188e9a9273af71c6a1c0ebf88450d66ae606ac
Johan Svensson <johan.svensson93@outlook.com>
2018-05-22 18:05:45 +0200

    Readded user role management and profile pics

--- HackslashForum/HackslashForum/Controllers/AccountController.cs
+++ HackslashForum/HackslashForum/Controllers/AccountController.cs
@@ -23,6 +23,7 @@ namespace HackslashForum.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly RoleManager<ApplicationUser> _roleManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailSender _emailSender;
@@ -296,25 +297,18 @@ namespace HackslashForum.Controllers
             model.ApplicationUsers = _context.Users.ToList();
 
             var getUser = model.ApplicationUsers;
-            foreach(var i in getUser)
+            var userToPromote = getUser.FirstOrDefault(u => u.Id == userId);
+
+            foreach (var i in getUser)
             {
                 userId = i.Id;
             }
 
-            var userToPromote = getUser.FirstOrDefault(u => u.Id == userId);
-
-            if(User.IsInRole("Member"))
-            {
             await _userManager.AddToRoleAsync(userToPromote, "Admin");
             await _userManager.RemoveFromRoleAsync(userToPromote, "Member");
-            }
-            if(User.IsInRole("Admin"))
-            {
-                await _userManager.AddToRoleAsync(userToPromote, "Member");
-                await _userManager.RemoveFromRoleAsync(userToPromote, "Admin");
-            }
 
-           return RedirectToAction("Index", "Home");
+
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
