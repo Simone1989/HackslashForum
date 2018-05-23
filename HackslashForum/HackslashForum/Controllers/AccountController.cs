7e37ae032fec76259227163dc2133e43da44a39c
Johan Svensson <johan.svensson93@outlook.com>
2018-05-23 13:30:58 +0200

    BugFix: Making users to Admin works
    Fixed Bug where dropdownlist would not select right user.

--- HackslashForum/HackslashForum/Controllers/AccountController.cs
+++ HackslashForum/HackslashForum/Controllers/AccountController.cs
@@ -291,28 +291,19 @@ namespace HackslashForum.Controllers
         [Authorize(Roles = "Admin")]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> RegisterRole(RoleViewModel model, string userId)
+        public async Task<IActionResult> RegisterRole(RoleViewModel model, string userName)
         {
             model.Roles = _context.Roles.ToList();
             model.ApplicationUsers = _context.Users.ToList();
 
-            var getUser = model.ApplicationUsers;
+            userName = model.SelectedUserId;
+            var userToPromote = model.ApplicationUsers.FirstOrDefault(u => u.UserName == userName);
 
-            foreach (var i in getUser)
-            {
-                userId = i.Id;
-            }
-
-            var userToPromote = getUser.FirstOrDefault(u => u.Id == userId);
-            if (User.IsInRole("Admin"))
-            {
-                await _userManager.AddToRoleAsync(userToPromote, "Admin");
-                await _userManager.RemoveFromRoleAsync(userToPromote, "Member");
-
-            }
+            await _userManager.AddToRoleAsync(userToPromote, "Admin");
+            await _userManager.RemoveFromRoleAsync(userToPromote, "Member");
 
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index","Home");
         }
 
         [HttpPost]
