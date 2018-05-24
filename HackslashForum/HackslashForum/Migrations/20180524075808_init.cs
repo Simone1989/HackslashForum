719f4d232579e94add3aa5475b2f2204150ad495
Johan Svensson <johan.svensson93@outlook.com>
2018-05-24 10:50:12 +0200

    Merge branch 'DemoteUser' into developer

--- HackslashForum/HackslashForum/Migrations/20180524075808_init.cs
+++ HackslashForum/HackslashForum/Migrations/20180524075808_init.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace HackslashForum.Migrations
 {
-    public partial class AddedUserIdToCommentAndPost : Migration
+    public partial class init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
